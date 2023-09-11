130003912633bc3561d7644058188f78fb60780b
rajxode <rajsh1202@gmail.com>
2023-09-11 16:35:28 +0530

    create and render home page for signin, along with route and controller

--- index.js
+++ index.js
@@ -7,5 +7,25 @@ const {PORT} = process.env;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded()); 
+app.use(express.static('assets'));
+
+// importing layouts 
+const expressLayouts =  require('express-ejs-layouts');
+
+// using layouts
+app.use(expressLayouts);
+
+// extracting stylesheets and scripts for individual pages
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+
+// setting view engine as ejs and defining its path
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.use('/',require('./routes'));
 
 app.listen(PORT,() => console.log(`Server is running on port: ${PORT}`));
